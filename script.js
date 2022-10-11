fd885a63dcc18d14b2fe4466bd0272fe77d5d901
Chidubem Akpu <stanleychidubemakpu@gmail.com>
2022-10-11 18:21:37 +0100

    shadow DOM...

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Use the fetch api to populate DOM.
+
 let theTag = document.querySelector('select');
  if (!localStorage.length){
     let myCountries = async (url) =>{
@@ -35,7 +37,82 @@ let theTag = document.querySelector('select');
         theTag.append(option);
     })
 }
+
+//Build my own web components.
+
+class TimeFormatted extends HTMLElement{
+    render(){
+        let date = Date.now();
+        this.setAttribute('date-time', date)
+        this.innerHTML = new Intl.DateTimeFormat('default', {
+            year: this.getAttribute('year') || undefined,
+            month: this.getAttribute('Month') || undefined,
+            day: this.getAttribute('day') || undefined,
+            hour: this.getAttribute('hour') || undefined,
+            minute: this.getAttribute('minute') || undefined,
+            second: this.getAttribute('second') || undefined,
+            timeZoneName: this.getAttribute('time-zone-name') || undefined,
+        }).format(date)
+    }
+
+    connectedCallback() {
+        if (this.rendered){
+            return 
+        }
+        this.render()
+        this.rendered  = true;
+        setInterval(() => this.render(), 1000)
+    }
+
+    static get observedAttributes(){
+        return ['date-time', 'year', 'month', 'day', 'hour', 'minute', 'time-zone-name']
+    }
+
+    attributeChangedCallback(name, oldValue, newValue){
+        this.render()
+    }
+}
+
+
+class HelloButton extends HTMLButtonElement{
+    constructor(){
+        super();
+        this.theStuff = 'Hello'
+        this.addEventListener('click', () => alert(this.theStuff));
+    }
+}
+
+class HiButton extends HelloButton{
+    constructor(){
+        super();
+        this.theStuff = 'Hi!';
+    }
+}
+
+customElements.define('hello-button', HelloButton, {extends: 'button'});
+customElements.define('hi-button', HiButton, {extends: 'button'});
+
+// shadow DOM
+
+
+customElements.define('show-hello', class extends HTMLElement{
+    connectedCallback(){
+        const shadow = this.attachShadow({mode: 'open'});
+        shadow.innerHTML = `<p>Hello. ${this.getAttribute('name')} </p>`
+    }
+})
+
+
+elem.onclick = function() {
+    elem.attachShadow({mode: 'open'});
+    elem.shadowRoot.append(tmpl.content.cloneNode(true));
+    elem.shadowRoot.getElementById('message').innerHTML = 'Hello from the shadowa'
+
+}
+
  
+
+
 
 
 
